04ec8500d093aba82c7f20215c31058d3b74d563
Genival Jr <moreno.alado@gmail.com>
2012-10-22 15:46:16 -0300

    Upado os arquivos

---
+++ footer.php
@@ -0,0 +1,24 @@
+		<div id="faixa">
+        	<ul class="centraliza">
+        		<li><a href=""><img src="<?php servidor();?>images/menu_footer/1_off.png" alt="" class="botao"></a></li>
+                <li>|</li>
+        		<li><a href=""><img src="<?php servidor();?>images/menu_footer/2_off.png" alt="" class="botao"></a></li>
+                <li>|</li>
+        		<li><a href=""><img src="<?php servidor();?>images/menu_footer/3_off.png" alt="" class="botao"></a></li>
+                <li>|</li>
+        		<li><a href=""><img src="<?php servidor();?>images/menu_footer/4_off.png" alt="" class="botao"></a></li>
+                <li>|</li>
+        		<li><a href=""><img src="<?php servidor();?>images/menu_footer/5_off.png" alt="" class="botao"></a></li>
+        	</ul>
+        </div>
+        <div class="centraliza">
+	        <div id="txt">
+				<?php $data=date("Y"); echo $data; ?> | Restaurante Piracema Todos os direitos reservados. <br>
+				Avenida Mendonça Furtado, 73 | Santarém - PA        
+	        </div>
+            <div id="redes">
+            	<a href=""><img src="<?php servidor();?>images/redes/1_off.png" alt="" class="botao"></a>
+                <a href=""><img src="<?php servidor();?>images/redes/2_off.png" alt="" class="botao"></a>
+            </div>
+            <a href="http://talentodesign.com.br/"><img src="images/talento.png" alt="" id="talento"></a>
+    	</div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("header.php");
+$guia[1]="current";
+ ?>
+    <title>Restaurante Piracema</title>
+  </head>
+  <body>
+    <div id="header">
+    	<?php include("topo.php"); ?>
+    </div>
+    <div id="slider_area">
+    	<div id="slider" class="centraliza">
+        	<img src="<?php servidor();?>images/slider_home/1.jpg" alt="">
+            <img src="<?php servidor();?>images/slider_home/2.jpg" alt="">
+        </div>
+    </div>
+    <div id="content">
+    	<div id="home" class="centraliza">
+        </div>
+    </div>
+    
+    <div id="footer">
+    	<?php include("footer.php"); ?>
+    </div>
+  </body>
+</html>
+
